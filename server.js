97c14761246e7d09822baf4dbeca6588ed3dc310
ramashish786 <ramashishsahani786@gmail.com>
2021-07-10 13:19:52 +0530

    udated server

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(bodyparser.urlencoded({
     extended: true
 }));
 app.use(bodyparser.json());
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(__dirname + '/views/image'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views', 'employee', 'addOrEdit.hbs'));
+});
 app.use(express.static(__dirname + '/views/image'));
 app.set('views',path.join(__dirname,'/views/'));
 app.engine('hbs',exphbs({extname:'hbs',defaultLayout:'mainLayout',layoutDir: __dirname+"/views/layouts/", handlebars: allowInsecurePrototypeAccess(Handlebars)}));
